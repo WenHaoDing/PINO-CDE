3f0688ca5d2bc0b4cce9b7f96659ee9a50a30c68
Hedawl <95233376+Hedawl@users.noreply.github.com>
2022-08-29 18:03:14 +0800

    Delete train_utils directory

--- train_utils/solution_extension.py
+++
@@ -1,8 +0,0 @@
-import torch
-import numpy as np
-import os
-
-def FDD_Extension(u, dt):
-    du = (u[:, 2:, :] - u[:, :-2, :]) / (2 * dt)
-    ddu = (u[:, 2:, :] - 2 * u[:, 1:-1, :] + u[:, :-2, :]) / (dt ** 2)
-    return du, ddu
